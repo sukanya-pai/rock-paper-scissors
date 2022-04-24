99207a84ffcddf181954c11bdf5b44e2ec0499a9
sukanya-pai <sukanya.pai1011@gmail.com>
2022-04-24 23:55:07 +0530

    fixed allowed hosts

--- rock_paper_scissors_game/rock_paper_scissors_game/settings.py
+++ rock_paper_scissors_game/rock_paper_scissors_game/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n)psq84g43j5wheri=h=a(-cjie+gxkjq50i_xefffjq__(qh3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
