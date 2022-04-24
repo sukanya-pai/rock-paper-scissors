50bba3973c4dece90019cd560a9022cc55e415d1
sukanya-pai <sukanya.pai1011@gmail.com>
2022-04-25 00:44:53 +0530

    updated wsgi file

--- rock_paper_scissors_game/rock_paper_scissors_game/wsgi.py
+++ rock_paper_scissors_game/rock_paper_scissors_game/wsgi.py
@@ -15,5 +15,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rock_paper_scissors_game.settin
 
 # application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+from whitenoise import WhiteNoise
+
+application = WhiteNoise(application)
